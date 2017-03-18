744659146b9bebf20ad309b7bf5fdd5bbe56fab5
Emil Gjørup <limemloh@gmail.com>
2017-03-18 14:25:01 +0100

    added another way of defining routes

--- examples/simple/src/app.ts
+++ examples/simple/src/app.ts
@@ -2,7 +2,8 @@ import { combine } from "jabz";
 import { Stream, Now, Behavior } from "hareactive";
 import { elements, component, Component } from "@funkia/funnel";
 const { h1, span, input, button, div } = elements;
-import { locationHashB, navigate, parsePathParams } from "../../../src/router";
+import { locationHashB, navigate, parsePathParams, routePath } from "../../../src/router";
+
 
 type FromView = {
   listBtn: Stream<any>
@@ -21,17 +22,15 @@ const menu = component<ToView, FromView, Out>(
   function* model({ listBtn, viewBtn }) {
     const navs = combine(listBtn.mapTo("/list"), viewBtn.mapTo("/view"));
     yield navigate(navs);
-    const params = parsePathParams("/:item", locationHashB);
-    const content = params.map(({item}) => {
-      switch (item) {
-        case "list":
-        return h1("ListContent");
-        case "view":
-        return span("ViewContent").chain(() => input());
-        default:
-        return h1("404");
-      }
-    })
+
+    locationHashB.subscribe(console.log);
+    const content = routePath({
+      "/list/:userId/profile": (restUrl, params) => h1("ListContent").chain(() => span(restUrl + " : " + JSON.stringify(params))),
+      "/view": (restUrl, params) => h1("ViewContent").chain(() => span(restUrl + " : " + JSON.stringify(params))),
+      "*": (restUrl, params) => h1("404").chain(() => span(restUrl + " : " + JSON.stringify(params)))
+    }, locationHashB);
+
+
     return [{ content }, {}];
   }, ({ content }) => [
     content,
@@ -41,7 +40,8 @@ const menu = component<ToView, FromView, Out>(
 
 export const main = component<{ l: Behavior<string> }, { }, Out>(
   function model({ }) {
-    const a = parsePathParams("/users", locationHashB);
+    //const a = parsePathParams("/users", locationHashB);
     
     return Now.of([{ l: locationHashB }, {}]);
   }, ({ l }) => h1(["locationB: ", l]).chain(() => menu));
+    
--- src/router.ts
+++ src/router.ts
@@ -18,11 +18,20 @@ export function navigate(urlStream: Stream<string>): Now<Stream<string>> {
 export const locationHashB = sink(takeUntilRight("#", window.location.hash));
 window.addEventListener("hashchange", (evt) => locationHashB.push(takeUntilRight("#", evt.newURL)), false);
 
-function readParams(paramRecord: Record<string, number>, path: string): Record<string, string> {
-  const pathVars = path.split("/");
+function readParams(pattern: string, path: string): Record<string, string> {
+  const patternParts = pattern.split("/");
+  let paramRecord: Record<string, number> = {};
+  for (let i = 0; i < patternParts.length; i++) {
+    const part = patternParts[i];
+    if (fst(part) === ":") {
+      paramRecord[part.substr(1)] = i;
+    }
+  }
+
+  const pathParts = path.split("/");
   let params: Record<string, string> = {};
   for (const param of Object.keys(paramRecord)) {
-    params[param] = pathVars[paramRecord[param]];
+    params[param] = pathParts[paramRecord[param]];
   }
   return params;
 }
@@ -34,13 +43,63 @@ function readParams(paramRecord: Record<string, number>, path: string): Record<s
  * @param locationBehavior A behavior describing the current location.
  */
 export function parsePathParams(pattern: string, locationBehavior: Behavior<string>): Behavior<Record<string, string>> {
-  const parts = pattern.split("/");
-  let paramRecord: Record<string, number> = {};
-  for (let i = 0; i < parts.length; i++) {
-    const part = parts[i];
+  return locationBehavior.map((location) => readParams(pattern, location));
+}
+
+type PathHandler<A> = (restUrl: string, params: Record<string, string>) => A;
+
+type ParsedPathPattern<A> = {
+  path: string[];
+  params: Record<string, number>;
+  length: number;
+  handler: PathHandler<A>
+};
+
+function parsePathPattern<A>(pattern: string, handler: PathHandler<A>): ParsedPathPattern<A> {
+  const patternParts = pattern.split("/");
+  let p: ParsedPathPattern<A> = {
+    path: [],
+    params: {},
+    length: patternParts.length,
+    handler
+  };
+  if (pattern === "*") {
+    return p;
+  }
+  for (let i = 0; i < patternParts.length; i++) {
+    const part = patternParts[i];
     if (fst(part) === ":") {
-      paramRecord[part.substr(1)] = i;
+      p.params[part.substr(1)] = i;
+    } else {
+      p.path[i] = part;
     }
   }
-  return locationBehavior.map((location) => readParams(paramRecord, location));
+  return p;
+}
+
+type Routes<A> = Record<string, (rest: string, params: Record<string, string>) => A>;
+
+/**
+ * Takes a description of the routes, a behavior of the current location and returns a
+ * behavior with the result of parsing the location according to the pattern.
+ * @param routes A description of the routes, in the form {"/route/:urlParam"; (restUrl, params) => result}
+ * @param locationBehavior A behavior describing the current location.
+ */
+export function routePath<A>(routes: Routes<A>, locationBehavior: Behavior<string>): Behavior<A> {
+  const parsedRoutes = Object.keys(routes).map((path) => parsePathPattern(path, routes[path]));
+
+  return locationBehavior.map((location) => {
+    const locationParts = location.split("/");
+    const match = parsedRoutes.find(({ path }: ParsedPathPattern<A>) => path.every((part, index) => {
+      console.log(part, locationParts[index]);
+      return part === locationParts[index];
+    }));
+    const rest = "/" + locationParts.slice(match.length).join("/");
+
+    let params: Record<string, string> = {};
+    for (const key of Object.keys(match.params)) {
+      params[key] = locationParts[match.params[key]];
+    }
+    return match.handler(rest, params);
+  });
 }
